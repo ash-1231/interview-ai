febb314f8ab26918fddc4a97265eab34bd112c92
ashutosh <ashutoshpro11@gmail.com>
2026-05-04 06:50:50 +0530

    Change API base URL to use environment variable

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+const API = import.meta.env.VITE_API_URL;
+
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: API,
     withCredentials: true,
-})
+});
 
 
 /**
