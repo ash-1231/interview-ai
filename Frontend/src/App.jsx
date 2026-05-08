be9b553ce40a52dd81087e8c26c8d5410f84ceb5
ashutosh <ashutoshpro11@gmail.com>
2026-05-08 22:22:46 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,8 @@
-import { RouterProvider } from "react-router"
-import { router } from "./app.routes.jsx"
-import { AuthProvider } from "./features/auth/auth.context.jsx"
-import { InterviewProvider } from "./features/interview/interview.context.jsx"
+import { RouterProvider } from "react-router";
+import { router } from "./app.routes.jsx";
 
 function App() {
- 
-
-  return (
-    <AuthProvider>
-      <InterviewProvider>
-         <RouterProvider router={router}/>
-      </InterviewProvider>
-    </AuthProvider>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
