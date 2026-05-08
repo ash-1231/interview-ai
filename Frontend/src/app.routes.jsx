7bd71168d706778bb13823ddcd29c8e7a3c59a3c
ashutosh <ashutoshpro11@gmail.com>
2026-05-08 23:10:48 +0530

    Update app.routes.jsx

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -1,44 +1,26 @@
-import { createBrowserRouter, Outlet } from "react-router";
+import { createBrowserRouter } from "react-router";
 import Login from "./features/auth/pages/Login";
 import Register from "./features/auth/pages/Register";
 import Protected from "./features/auth/components/Protected";
 import Home from "./features/interview/pages/Home";
 import Interview from "./features/interview/pages/Interview";
 
-import { AuthProvider } from "./features/auth/auth.context.jsx";
-import { InterviewProvider } from "./features/interview/interview.context.jsx";
-
-// 👇 THIS IS THE IMPORTANT PART
-const RootLayout = () => {
-  return (
-    <AuthProvider>
-      <InterviewProvider>
-        <Outlet />
-      </InterviewProvider>
-    </AuthProvider>
-  );
-};
 
 export const router = createBrowserRouter([
-  {
-    element: <RootLayout />,   // 👈 wraps everything
-    children: [
-      {
+    {
         path: "/login",
         element: <Login />
-      },
-      {
+    },
+    {
         path: "/register",
         element: <Register />
-      },
-      {
+    },
+    {
         path: "/",
         element: <Protected><Home /></Protected>
-      },
-      {
-        path: "/interview/:interviewId",
+    },
+    {
+        path:"/interview/:interviewId",
         element: <Protected><Interview /></Protected>
-      }
-    ]
-  }
-]);
+    }
+])
